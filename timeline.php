4377f6943f2a0944678e5165387f60248a3d87a4
tomoya <tomoya@tomoya-MacbookAir.local>
2015-07-08 18:06:31 +0900

    changed

--- timeline.php
+++ timeline.php
@@ -11,6 +11,7 @@
 <br>
 
 
+<!-- //tweet form-->
 <?php $userName = $_POST['userName'] ?>
 <form method="post" action="post.php"><div class="post">
 	<p><input name="userName" type="hidden"  value=<?php echo $userName?>></p>
@@ -18,24 +19,15 @@
 	<p id="post_button"><input name="submit" type="submit" value="ささやく"></p>
 </div></form>
 
+<!-- //get tweets -->
 <?php require 'get.php'; ?>
 
-<?php
+<!-- //print tweets -->
 <div id="tweets_area">
-	foreach ($docs as $obj){
-		<div class="posts">
-			<img src="images/icon1.jpg" class="icon" width=50 height50>
-			<p>echo var_dump($obj);</p>
-		</div>
-	} 
-</div>
-?>
-
-<div id="tweets_area">
-<?php foreach ($docs as $obj){ ?>
+<?php foreach ($tweetList as $tweet){ ?>
 	<div class="posts">
 		<img src="images/icon1.jpg" class="icon" width=50 height50>
-		<p><?php echo var_dump($obj);?></p>
+		<p><?php echo var_dump($tweet);?></p>
 	</div>
 <?php } ?>
 </div>
