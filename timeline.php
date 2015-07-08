86a0f7535444dbb21d2dd5e311c6c1d47d126e42
tomoya <tomoya@tomoya-MacbookAir.local>
2015-07-08 20:08:03 +0900

    changed

--- timeline.php
+++ timeline.php
@@ -14,7 +14,7 @@
 <!-- //tweet form-->
 <form method="post" action="post.php"><div class="post">
 	<input name="userName" type="hidden"  value=<?php echo $userName?>><!--userNamer値を渡す-->
-	<input name="content" type="text" id="post_area"></textarea></p>
+	<input name="content" type="text" id="post_area" style="width:200px;height:50px;"></textarea></p>
 	<p id="post_button"><input name="submit" type="submit" value="ささやく"></p>
 </div></form>
 
